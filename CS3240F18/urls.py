0fec80b483f174dadc86ca2753e933be998a7192
Ian Reyes <ianreyes99@gmail.com>
2021-02-23 03:15:20 -0500

    Added landing page

--- CS3240F18/urls.py
+++ CS3240F18/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('textbookexchange.urls')),
     path('textbook/', include('textbookexchange.urls')),
     path('admin/', admin.site.urls),
 ]
--- textbookexchange/views.py
+++ textbookexchange/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.staticfiles.views import serve
+import os
 
 def index(request):
-    return HttpResponse("Hi all class!!! Hello, world. You are my everything!  :heart:")
+    
+    return HttpResponse("B-30 Club")
